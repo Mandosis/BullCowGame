5be609e241b200c0a91a6b84a514cf6826f143e9
Chris Rabuse <rab233@live.com>
2016-07-26 22:13:55 -0500

    Refactor code into functions

--- Section_02/Section_02.cpp
+++ Section_02/Section_02.cpp
@@ -2,10 +2,42 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
+#include <string>
 
+using namespace std;
 
-int main()
-{
-    return 0;
+void printIntro();
+string getGuessAndPrintBack();
+
+int main() {
+
+	constexpr int NUMBER_OF_TURNS = 5;
+
+	printIntro();
+
+	for (int it = 0; it < NUMBER_OF_TURNS; it++) {
+		getGuessAndPrintBack();
+		cout << endl;
+	}
+
+	return 0;
 }
 
+void printIntro() {
+	constexpr int WORD_LENGTH = 5;
+
+	cout << "welcome to Bulls and Cows, a fun word game." << endl;
+	cout << "Can you guess the " << WORD_LENGTH << " letter isogram I'm thinking of?" << endl << endl;
+}
+
+string getGuessAndPrintBack() {
+	string guess = "";
+
+	cout << "Enter your guess: ";
+	getline(cin, guess);
+
+	cout << "Your guess was: " << guess << endl;
+
+	return guess;
+}
